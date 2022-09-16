1cee8378b7375f2f368e9bbcf26760ca8a1d2ed9
a21manuelsg <a21manuelsg@iessanclemente.net>
2022-09-16 12:29:00 +0200

    Primer ejemplo en el controlador, bucle foreach

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
     def index
-    
+        @products = Product.all() # Nos devuelve todos los productos, y lo almacenamos en una variable global o de instancia
     end
 end
